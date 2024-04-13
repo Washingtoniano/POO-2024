978dd5bffac3f9cbe2d39c47bea6b0e57ca0be54
Alfredo <corderocccc@gmail.com>
2024-04-13 03:11:21 -0300

    A testear

--- Unidad 2/Practica 1/CajaDeAhorro.py
+++ Unidad 2/Practica 1/CajaDeAhorro.py
@@ -9,30 +9,80 @@ class CajaDeAhorro():
         self.__cuil=cuil
         self.__apellido=apellido
         self.__nombre=nombre
-        self.__saldo=saldo
+        #self.__saldo=saldo
+        # if ncuenta=='':
+        #     self.__ncuenta=None
+        # else:
+        #     self.__ncuenta=ncuenta
+
+        # if cuil =='':
+        #     self.__cuil=None
+        # else:
+        #     self.__cuil=cuil
+
+        # if apellido=='':
+        #     self.__apellido=None
+        # else:
+        #     self.__apellido=apellido
+
+        # if nombre=='':
+        #     self.__nombre=None
+        # else:
+        #     self.__nombre=nombre
+
+        if saldo=='':
+             self.__saldo=0.0
+        else:
+             self.__saldo=float(saldo)
     #def __str__(self):
        # return("Los datos son\n n°de cuenta:{} cuil: {} apellido:{} nombre:{} saldo:{}".format(self.__ncuenta,self.__cuil,self.__apellido,self.__nombre,self.__saldo))
     def mostrar(self):
         print("Los datos son\n n°de cuenta:{} cuil: {} apellido:{} nombre:{} saldo:{}".format(self.__ncuenta,self.__cuil,self.__apellido,self.__nombre,self.__saldo))
+    def cargarA(self):
+        self.__apellido()
     def extraer(self,monto):
         resultado=-1
+        print("{}".format(type(self.__saldo)))
         if(self.__saldo>=monto):
             self.__saldo=self.__saldo-monto
             resultado=self.__saldo
         return resultado
 
     def depositar(self,monto):
-        valor=-1
+        #valor=-1
         if(monto>0):
             self.__saldo=self.__saldo+monto
-            valor=self.__saldo
-        return valor
+            #valor=self.__saldo
+        return self.__saldo
     def comprobar(self, cuil):
         partes=cuil.split('-')
-        if
-        
-
-
-        
-                
-            
+        band=False
+        if ((int (partes[0])==27) or (int (partes[0])==30) or (int (partes[0])==20)):
+            if (int(len(partes[1]))==8):
+                if (int(len(partes[2]))==1):
+                    acum=0
+                    
+                    numeros=[5,4,3,2,7,6,5,4,3,2]
+                    cant=partes[0]+partes[1]
+                    cant
+                    for i in range (len(cant)):
+                        acum=acum+(int(cant[i])*numeros[i])
+                        print("{}".format(acum))
+                    val=acum/11
+                    rest=acum-(int(val)*11)
+                    print("Val {}".format(val))
+                    print("Rest {}".format(rest))
+                    if rest==0:
+                        if int(partes[2])==0:
+                            band=True
+                    elif rest==1:
+                        if int(partes[0])==20:
+                            if int(partes[2])==9:
+                                band=True
+                        elif int(partes[0])==27:
+                            if int(partes[2])==4:
+                                band=True
+                        else:
+                            if int(partes[2])==11-rest:
+                                band=True
+        return band
--- Unidad 2/Practica 1/Test.py
+++ Unidad 2/Practica 1/Test.py
@@ -1,17 +1,45 @@
 from CajaDeAhorro import CajaDeAhorro
+
 def test():
-    N=input("Numerp de Cuenta")
-    C=input("cuil")
-    A=input("Apellido")
-    No=input("Nombre")
-    S=float(input("saldo"))
+    N=input("Numerp de Cuenta ")
+    C=input("cuil ")
+    A=input("Apellido ")
+    No=input("Nombre ")
+    S=input("saldo ")
 
     caja=CajaDeAhorro(N,C,A,No,S)
+
+    # partes=(input("Ingrese el cuil a verificar(No olvide los -)"))
+    # partes=partes.split('-')
+    # for i in range (len(partes)):
+    #     print("{}".format(partes[i]))
+    # for j in range (len(partes[1])):
+    #     print("{}".format(partes[1][j]))
     caja.mostrar()
-    monto=float(input("Ingrese un monto a extraer"))
-    ext=caja.extraer()
+    monto=(input("Ingrese un monto a extraer "))
+    # if monto=='':
+    #     monto=0.0
+    # else:
+    #     float(monto)
+    # ext=caja.extraer(monto)
+    ext=caja.extraer(float(monto))
     if ext != -1:
         print("El monto actualizado es {}".format(ext))
-    caja.depositar()
-    caja.comprobar()
+    else:
+        print("saldo Insuficiente")
+    monto=input("Ingrese un monto a depositar ")
+    # if monto=='':
+    #     monto=0.0
+    # else:
+    #     float(monto)
+    # val=caja.depositar(monto)
+    val=caja.depositar(float(monto))
+    print("El monto actualizado es {}".format(val))
+    
+    p=caja.comprobar(input("Ingrese el cuil a verificar(No olvide los -) "))
+    if p==False:
+        print("Cuil invalido")
+    else:
+        print("cuil valido")
+    
     #print("{}".format(caja))
